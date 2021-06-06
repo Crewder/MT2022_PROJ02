4e6cb794f5d735cb0bc5009820ef77e61eaa1b46
Steven <steven.nativel@orange.fr>
2021-06-06 16:49:10 +0200

    fix folder exist

--- www/upload.php
+++ www/upload.php
@@ -3,14 +3,17 @@
     if(isset($_FILES['picture']))
     { 
         $folder = 'upload/';
-        $file = basename($_FILES['picture']['name']);
-        if(move_uploaded_file($_FILES['picture']['tmp_name'], $folder . $file))
-        {
-            echo 'Upload effectué avec succès !';
-        }
-        else
-        {
-            echo 'Echec de l\'upload !';
+        if(!file_exists($folder)){
+            mkdir($folder);
+            $fileName = basename($_FILES['picture']['name']);
+            if(move_uploaded_file($_FILES['picture']['tmp_name'], $folder . $fileName))
+            {
+                echo 'Upload effectué avec succès !';
+            }
+            else
+            {
+                echo 'Echec de l\'upload !';
+            }
         }
     }
 
